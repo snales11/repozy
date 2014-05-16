076be225442286465ceddd433debe8f1b873e0d3
snales11 <snales11@mail.ru>
2014-05-16 21:07:22 +0700

    create order

--- protected/views/site/product_rez.php
+++ protected/views/site/product_rez.php
@@ -3,6 +3,10 @@
  $product = get_product($id);
  
   ?>
+  <script type="text/javascript">
+  
+  
+  </script>
   <div class="right">  
   
    	<img align="left" style="padding: 5px 10px 5px 5px;" src="images/tovar/<?php echo $product['image']?> " alt="<?php echo $product['title'] ?>" width="210" class="items" />
@@ -10,6 +14,8 @@
    <div id="title"><?php echo $product['title']?></div> 
    <div class="price">Цена: <?php echo +$product['price'];?>$</a></div>
    <div id="description"><?php echo $product['description']?></div>
-    
-  
-</div>
+   <br />
+   <div><a href="index.php?r=orders/index&str=<?php echo $product['id']?>"><img src ="images/button2.gif"/></a></div>
+     
+ </div>
+
